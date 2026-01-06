d38121e672f3ba53f3e3d4c7c8f35bb1fffc14ab
aroncitoper-cibernético <aroncithoper@gmail.com>
2026-01-05 20:39:15 -0600

    Fix PWA install

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,2 @@
-// public/sw.js
-self.addEventListener('install', (e) => {
-  // Fuerza a que el Service Worker se active de inmediato sin esperar
-  self.skipWaiting();
-  console.log('Service Worker: Instalado y Activado');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Mantiene la App funcionando mientras haya internet
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
-  );
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
